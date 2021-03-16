f4fb1f8fbfc11070039339ed043da5812088e36e
John Helveston <john.helveston@gmail.com>
2021-03-16 13:54:26 -0400

    updated mp3 due dates and project templates

--- projects/mini-project-3/load_data.R
+++ projects/mini-project-3/load_data.R
@@ -1,18 +1,3 @@
-# Get the Data
-
-# Read in with tidytuesdayR package
-# Install from CRAN via: install.packages("tidytuesdayR")
-# This loads the readme and all the datasets for the week of interest
-
-# Either ISO-8601 date or year/week works!
-
-tuesdata <- tidytuesdayR::tt_load('2020-09-01')
-tuesdata <- tidytuesdayR::tt_load(2020, week = 36)
-
-key_crop_yields <- tuesdata$key_crop_yields
-
-# Or read in the data manually
-
 key_crop_yields <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-01/key_crop_yields.csv')
 fertilizer <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-01/cereal_crop_yield_vs_fertilizer_application.csv')
 tractors <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-01/cereal_yields_vs_tractor_inputs_in_agriculture.csv')
